95f9ca503774810597047151febca5c5d4a19f41
MAIRAJ AFRIDI <mairajafridi5@gmail.com>
2023-10-07 19:49:27 +0500

    create the ActivateCashback section

--- src/components/Home/Error.tsx
+++ src/components/Home/Error.tsx
@@ -6,16 +6,16 @@ const Error = () => {
 
     <div className='gap-8 w-full flex flex-col items-center justify-center'>
         <img
-            className="w-[210px] h-[230px] object-cover pointer-events-none rounded-xl"
-            src="/Assets/Errorr.jpeg"
+            className="w-[150px] sm:w-[210px] h-[150px] sm:h-[230px] object-cover pointer-events-none rounded-xl"
+            src="/Assets/Error.png"
             alt=""
         />
-        <div className='flex  flex-col items-center justify-center gap-[8px]'>
-            <h1 className='text-[30px] font-jakarta font-semibold text-black-secondary '>Cashback is not available on this site</h1>
-            <p className='font-medium text-[18px] leading-5 text-black-off font-manrope text-center '>If you believe that we are supporting this website, please <br/> head to 'Stores' and activate cashback manually.</p>
+        <div className='flex  flex-col items-center justify-center text-center gap-[8px]'>
+            <h1 className='text-[18px] sm:text-[30px] font-jakarta font-semibold text-black-secondary '>Cashback is not available on this site</h1>
+            <p className='font-medium text-[13px] sm:text-[18px] leading-5 text-black-off font-manrope '>If you believe that we are supporting this website, please <br/> head to 'Stores' and activate cashback manually.</p>
         </div>
         <div className='inline-flex rounded-[12px]  px-[12px] sm:px-[42px] py-[3px] sm:py-[6px]  justify-center items-center gap-[8px] cash-box '>
-    <h1 className='text-[24px] font-semibold'>View Stores</h1>
+    <h1 className='text-20px sm:text-[24px] font-semibold'>View Stores</h1>
 </div>
     </div>
 </section>
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,46 +1,12 @@
 import React from 'react'
-import { AiFillDollarCircle, AiOutlineExclamationCircle } from 'react-icons/ai'
-import { BsStars } from 'react-icons/bs'
-import { MdOutlineKeyboardArrowRight } from 'react-icons/md'
-import Error from './Error';
+import ActivateCashback from './ActivateCashback';
 
 
 const Home = () => {
-    return (
-        <section className='rounded-md shadow-md  min-h-[521px] h-full w-full gap-4 sm:gap-8 bg-white-main flex flex-col items-center justify-center '>
-
-            {/* logo................> */}
-            {/* <div className=' w-[296px] flex items-center justify-center'>
-                <img
-                    className="w-[210px] h-[210px] object-cover pointer-events-none rounded-xl"
-                    src="/Assets/Adidas.png"
-                    alt=""
-                />
-            </div> */}
-            {/* adidas title.....................> */}
-            {/* <div className='flex  flex-col items-center justify-center gap-[2px]'>
-                <h1 className='text-[30px] font-jakarta font-semibold text-black-secondary '>Adidas</h1>
-                <p className='font-medium text-[18px] leading-5 text-black-off font-manrope '>Apparel Company</p>
-            </div> */}
-            {/* CashBox............> */}
-            {/* <div className='inline-flex rounded-[32px]  px-[12px] sm:px-[18px] py-[3px] sm:py-[8px]  justify-center items-center gap-[8px] cash-box '>
-                <AiFillDollarCircle className='text-[17px] sm:text-[24px] text-success' />
-                <h1 className='text-[24px] text-success font-semibold'>Upto 10% Cashback</h1>
-                <AiOutlineExclamationCircle className='text-[24px] text-[#CDCDCD]  ' />
-            </div> */}
-
-            {/* Button............> */}
-            {/* <div className='inline-flex cursor-pointer  pl-14 sm:pl-40  pr-1 sm:pr-3 rounded-[8px] py-2 justify-center items-center gap-[8px] bg-yellow-400 shadow-md '>
-                <BsStars className='text-[17px] sm:text-[24px]' />
-                <h1 className=' text-[17px] sm:text-[24px] font-jakarta font-semibold '>Activate Cashback</h1>
-                <MdOutlineKeyboardArrowRight className=' text-[17px] sm:text-[24px] ml-12 sm:ml-24' />
-            </div> */}
-
-            {/* .............................404 error section.................. */}
-          <Error/>
-
-        </section>
-    )
+    return <>
+    <ActivateCashback  />
+    </>
+    
 }
 
 export default Home
