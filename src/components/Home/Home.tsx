ad4c64898f6dde7bf7fcad64d52754cac802c230
MAIRAJ AFRIDI <mairajafridi5@gmail.com>
2023-10-07 13:13:59 +0500

    Adidas logo set on home page

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -3,8 +3,12 @@ import React from 'react'
 const Home = () => {
   return (
     <section className='bg-white-main flex items-center justify-center '>
-        <div className='bg-gray-300 h-[101px] w-[296px] '>
-            <image href=''></image>
+        <div className=' mt-12   w-[296px] flex items-center justify-center'>
+        <img
+          className="w-[170px] h-[170px] object-cover pointer-events-none rounded-xl"
+          src="/Assets/Adidas.png"
+          alt=""
+        />
         </div>
         </section>
   )
